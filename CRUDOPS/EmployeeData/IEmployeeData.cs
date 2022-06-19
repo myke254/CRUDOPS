fba4391d2892bb27369afcb7e48457868b0750c3
myke254 <ngangamichael254@outlook.com>
2022-06-19 20:39:28 +0300

    Add project files.

---
+++ CRUDOPS/EmployeeData/IEmployeeData.cs
@@ -0,0 +1,17 @@
+﻿using CRUDOPS.Models;
+
+namespace CRUDOPS.EmployeeData
+{
+    public interface IEmployeeData
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        Employee CreateEmployee(Employee employee);
+
+        Employee UpdateEmployee(Employee employee);
+
+        void DeleteEmployee(Employee employee);
+    }
+}
---
+++ CRUDOPS/Models/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDOPS.Models
+{
+    public class EmployeeContext : DbContext
+    {
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
